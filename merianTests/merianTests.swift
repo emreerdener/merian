eafeef07d5bf805db737c56403f8d16cf64f9d99
emreerdener <erdener.emre@gmail.com>
2026-03-06 15:00:17 -0600

    Initial Commit

---
+++ merianTests/merianTests.swift
@@ -0,0 +1,17 @@
+//
+//  merianTests.swift
+//  merianTests
+//
+//  Created by Emre Erdener on 3/6/26.
+//
+
+import Testing
+@testable import merian
+
+struct merianTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
