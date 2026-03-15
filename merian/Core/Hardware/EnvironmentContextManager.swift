1928a84aabde6d30cbd8a1498f87dab04093eb63
emreerdener <erdener.emre@gmail.com>
2026-03-15 16:02:26 -0500

    fix: Resolve MKMapItem addressRepresentations compiler deprecations

--- merian/Core/Hardware/EnvironmentContextManager.swift
+++ merian/Core/Hardware/EnvironmentContextManager.swift
@@ -158,11 +158,11 @@ final class EnvironmentContextManager: NSObject, ObservableObject, CLLocationMan
             guard let request = MKReverseGeocodingRequest(location: location) else { return nil }
             let mapItems = try await request.mapItems
             
-            if let target = mapItems.first?.placemark {
-                if let locality = target.locality, let administrativeArea = target.administrativeArea {
-                    return "\(locality), \(administrativeArea)"
-                } else if let locality = target.locality {
-                    return locality
+            if let target = mapItems.first {
+                if let cityContext = target.addressRepresentations?.cityWithContext {
+                    return cityContext
+                } else if let cityName = target.addressRepresentations?.cityName {
+                    return cityName
                 } else if let name = target.name {
                     return name
                 }
