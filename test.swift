55a76fc6e74bbecba20518a15904c9c115b8a6d0
emreerdener <erdener.emre@gmail.com>
2026-03-15 23:33:19 -0500

    chore: delete build log and test Swift file.

--- test.swift
+++
@@ -1,9 +0,0 @@
-import Accelerate
-
-func test() {
-    var vBuffer = vImage_Buffer()
-    var histogram = [vImagePixelCount](repeating: 0, count: 256)
-    histogram.withUnsafeMutableBufferPointer { ptr in
-        let err = vImageHistogramCalculation_Planar8(&vBuffer, ptr.baseAddress!, vImage_Flags(kvImageNoFlags))
-    }
-}
